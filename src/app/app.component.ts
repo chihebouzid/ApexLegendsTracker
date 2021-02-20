e4ff822001bbf3fdd4ceae1887e2efa6698f6581
chihebouzid <chihebouzid@gmail.com>
2021-02-20 10:57:43 +0100

    move logic from searchCompenent to appComponent

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,6 @@
 import { Component, Input } from '@angular/core';
 import { PlayerData } from './player-card/player-card.component';
+import { TrackerAPIService } from './services/trackerggAPI.service';
 
 @Component({
   selector: 'app-root',
@@ -11,9 +12,57 @@ export class AppComponent {
   players: PlayerData[] = [];
   showScrollBar: string = '';
 
-  addPlayer(players: PlayerData[]) {
-    this.players = players;
-    this.showScrollBar = this.players.length >= 3 ? 'scroll' : 'hidden';
-    console.log('app component players', players);
+  constructor(private trackerAPIService: TrackerAPIService) {}
+
+  getUserInput(player: string[]) {
+    this.getPlayerData(player[0], player[1]);
   }
+
+  getPlayerData(playerHandle: string, platform: string) {
+    this.trackerAPIService
+      .getPlayerData(playerHandle, platform)
+      .subscribe((data) => {
+        console.log(data);
+        let activeLegendBGImg = this.getActiveLegend(data.data.segments);
+        this.players.push(
+          this.convertToPlayerSchema(
+            data.data.segments[0].stats,
+            playerHandle,
+            activeLegendBGImg
+          )
+        );
+      });
+  }
+
+  convertToPlayerSchema(
+    stats: any,
+    playerHandle: string,
+    activeLegendBGImg: string
+  ) {
+    let playerData = {
+      name: playerHandle,
+      level: stats.level.displayValue,
+      kills: stats.kills.displayValue,
+      damage: stats.damage.displayValue,
+      rank: {
+        iconUrl: stats.rankScore.metadata.iconUrl,
+        rankName: stats.rankScore.metadata.rankName,
+      },
+      avtiveLegendBGImg: activeLegendBGImg,
+    };
+    return playerData;
+  }
+
+  getActiveLegend(data: any) {
+    let activeLegend = data.filter((legend: any) => {
+      return legend.metadata.isActive;
+    });
+    return activeLegend[0].metadata.bgImageUrl;
+  }
+
+  // addPlayer(players: PlayerData[]) {
+  //   this.players = players;
+  //   this.showScrollBar = this.players.length >= 3 ? 'scroll' : 'hidden';
+  //   console.log('app component players', players);
+  // }
 }
--- src/app/search-bar/search-bar.component.ts
+++ src/app/search-bar/search-bar.component.ts
@@ -21,52 +21,13 @@ export class SearchBarComponent implements OnInit {
     { value: 'psn', viewValue: 'Playstation' },
   ];
   players: PlayerData[] = [];
-  constructor(private trackerAPIService: TrackerAPIService) {}
-  @Output() playerAdded = new EventEmitter<PlayerData[]>();
+  constructor() {}
+  @Output() userInput = new EventEmitter<string[]>();
 
   ngOnInit(): void {}
 
-  getPlayerData(playerHandle: string, platform: string) {
-    this.trackerAPIService
-      .getPlayerData(playerHandle, platform)
-      .subscribe((data) => {
-        console.log('PlayerData:', data);
-        let activeLegendBGImg = this.getActiveLegend(data.data.segments);
-        this.players.push(
-          this.convertToPlayerSchema(
-            data.data.segments[0].stats,
-            playerHandle,
-            activeLegendBGImg
-          )
-        );
-        this.playerAdded.emit(this.players);
-      });
-  }
-
-  convertToPlayerSchema(
-    stats: any,
-    playerHandle: string,
-    activeLegendBGImg: string
-  ) {
-    let playerData = {
-      name: playerHandle,
-      level: stats.level.displayValue,
-      kills: stats.kills.displayValue,
-      damage: stats.damage.displayValue,
-      rank: {
-        iconUrl: stats.rankScore.metadata.iconUrl,
-        rankName: stats.rankScore.metadata.rankName,
-      },
-      avtiveLegendBGImg: activeLegendBGImg,
-    };
-    return playerData;
-  }
-
-  getActiveLegend(data: any) {
-    let activeLegend = data.filter((legend: any) => {
-      return legend.metadata.isActive;
-    });
-    console.log('activeLegend', activeLegend);
-    return activeLegend[0].metadata.bgImageUrl;
+  getUserInput(playerHandle: string, platform: string) {
+    let playerData = [playerHandle, platform];
+    this.userInput.emit(playerData);
   }
 }
